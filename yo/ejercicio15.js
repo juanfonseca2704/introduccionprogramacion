d08645abfe680dde211a5de93fc732decb512668
kt <juanfonseca2704@gmail.com>
2026-05-27 11:23:22 -0500

    clase27-05-26#1

--- yo/ejercicio15.js
+++ yo/ejercicio15.js
@@ -1,10 +1,14 @@
-let numer=[],n,i,turn=0,result,add,suma=0;
+let numer=[],n,i,turn=1,result,add,suma=0;
 i=parseInt(prompt("Ingrese la cantidad de numeros que desea registrar"));
 do{
-    n=parseInt(prompt("Ingrese numeros, turno N°"+(turn+1)));
+    n=parseInt(prompt("Ingrese numeros, turno N°"+(turn)));
     add=numer.push(n);
     turn++;
 }while(turn<=i)
+    turn=0;
 do{
-    suma=suma+numer[turn]
-}while(i);
+    suma=suma+numer[turn];
+    turn++;
+}while(turn<i);
+document.write("La suma es: "+suma+"<br>");
+document.write("Fue sumado: "+numer);
