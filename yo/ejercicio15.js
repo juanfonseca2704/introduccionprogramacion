704fbb0a7094f69a54185007296d17477629c812
kt <juanfonseca2704@gmail.com>
2026-05-27 11:03:38 -0500

    la misma pinche clase

--- yo/ejercicio15.js
+++ yo/ejercicio15.js
@@ -1,7 +1,10 @@
-let numer=[],n,i,turn=1,result;
+let numer=[],n,i,turn=1,result,add;
 i=parseInt(prompt("Ingrese la cantidad de numeros que desea registrar"));
 do{
     n=parseInt(prompt("Ingrese numeros, turno N°"+turn));
     add=numer.add(n);
     turn++;
-}while(turn<i)
+}while(turn!=i)
+do{
+
+}while(i);
